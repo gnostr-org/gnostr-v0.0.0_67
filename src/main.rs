04d02be65943f81fecdf80899cfa834f84559668
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-24 00:31:05 -0500

    src/main.rs: detect possible threads

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
+#![allow(unused)]
+#![allow(dead_code)]
+use std::convert::TryInto;
+use std::any::type_name;
+use std::mem::size_of;
+use std::{io, thread};
 use argparse::{ArgumentParser,Store};
-
 use gitminer::Gitminer;
 
 mod worker;
 mod gitminer;
 
-fn main() {
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
+fn convert_to_u32(v: usize) -> Option<i8> {
+    if v > (std::i8::MAX as i32).try_into().unwrap() {
+        None
+    } else {
+        Some(v as i8)
+    }
+}
+
+fn main() -> io::Result<()> {
+
+    let start = time::get_time();
+
+    let count = thread::available_parallelism()?.get();
+    assert!(count >= 1_usize);
+    println!("{}={}", type_of(count), (count as i32));
+
     let mut opts = gitminer::Options{
-        threads: 8,
+        threads: count.try_into().unwrap(),
         target:  "000000".to_string(),
         message: "default commit message".to_string(),
         repo:    ".".to_string(),
@@ -16,7 +40,6 @@ fn main() {
 
     parse_args_or_exit(&mut opts);
 
-    let start = time::get_time();
     let mut miner = match Gitminer::new(opts) {
         Ok(m)  => m,
         Err(e) => { panic!("Failed to start git miner: {}", e); }
@@ -29,6 +52,7 @@ fn main() {
 
     let duration = time::get_time() - start;
     println!("Success! Generated commit {} in {} seconds", hash, duration.num_seconds());
+    Ok(())
 }
 
 fn parse_args_or_exit(opts: &mut gitminer::Options) {
@@ -36,12 +60,12 @@ fn parse_args_or_exit(opts: &mut gitminer::Options) {
     ap.set_description("Generate git commit sha with a custom prefix");
 
     ap.refer(&mut opts.repo)
-        .add_argument("repository-path", Store, "Path to your git repository (required)")
-        .required();
+        .add_argument("repository-path", Store, "Path to your git repository (required)");
+        //.required();
 
     ap.refer(&mut opts.target)
-        .add_option(&["-p", "--prefix"], Store, "Desired commit prefix (required)")
-        .required();
+        .add_option(&["-p", "--prefix"], Store, "Desired commit prefix (required)");
+        //.required();
 
     ap.refer(&mut opts.threads)
         .add_option(&["-t", "--threads"], Store, "Number of worker threads to use (default 8)");
