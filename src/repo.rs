c6ca68de095ffdfde57447dd2f7fa75de19ae19c
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-12-26 22:01:46 -0500

    src/repo.rs: repo::state return RepositoryState enum

--- src/repo.rs
+++ src/repo.rs
@@ -1,9 +1,18 @@
 extern crate git2;
 use git2::Repository;
+use git2::RepositoryState;
 
-pub fn state() {
+pub trait ToString {
+    fn to_string(&self) -> String;
+}
+
+pub fn state() -> RepositoryState {
     let repo_root = std::env::args().nth(1).unwrap_or(".".to_string());
     let repo = Repository::open(repo_root.as_str()).expect("Couldn't open repository");
     //println!("{} state={:?}", repo.path().display(), repo.state());
     println!("state={:?}", repo.state());
+	if repo.state() == RepositoryState::Clean {
+		println!("clean {:?}", repo.state());
+	}
+	return repo.state()
 }
