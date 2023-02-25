eb6bdf3b12638205b168eb392f10a5a6c346738d
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-02-25 11:19:52 -0500

    make: cargo-* help

--- cargo.mk
+++ cargo.mk
@@ -1,11 +1,11 @@
-cargo-build:
+cargo-build:## 	cargo-build
 	@cargo b
-cargo-install:
+cargo-install:## 	cargo-install
 	@cargo install --path $(PWD)
 	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
-cargo-build-release:
+cargo-build-release:## 	cargo-build-release
 	@cargo b --release
-cargo-check:
+cargo-check:## 	cargo-check
 	@cargo c
 # vim: set noexpandtab:
 # vim: set setfiletype make
