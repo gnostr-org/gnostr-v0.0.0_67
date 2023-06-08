d4fe187782e5e1653b649bf8d2430dd5dfa5ced6
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-06-08 18:45:52 -0400

    loop.sh:

--- loop.sh
+++ loop.sh
@@ -1,63 +1,86 @@
 #!/bin/bash
-FULLPATH=`pwd`
-INCPATH=${FULLPATH%/*}
-#echo $FULLPATH
-#echo $INCPATH
+#
+declare -a LENGTH
+declare -a RELAYS
+declare -a FULLPATH
 
+FULLPATH=`pwd`
 source $FULLPATH/random-between.sh
-#min=0
-#max=1000
-#divisibleBy=1
-#
-randomBetween 0 100 1
-#echo "$randomBetweenAnswer"
-#
-#exit
-# Basic while loop
-#GET_RELAYS=$(curl https://api.nostr.watch/v1/online)
-#echo $GET_RELAYS
-#exit;
-#
+
+function get_relays(){
+
 RELAYS=$(curl 'https://api.nostr.watch/v1/online' |
     sed -e 's/[{}]/''/g' |
     sed -e 's/\[/''/g' |
     sed -e 's/\]/''/g' |
     sed -e 's/"//g' |
     awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}')
-
+    return $RELAYS
+}
+get_relays
 #echo $RELAYS
 
+echo "" > RELAYS.md
 for relay in $RELAYS; do
-    echo $relay
+    echo $relay >> RELAYS.md
     let LENGTH=$((LENGTH + 1))
 done
-#echo RELAYS=$LENGTH
-#exit
-counter=0
-limit=$LENGTH
-randomBetween 0 $LENGTH 1
+# randomBetween
+# echo randomBetweenAnswer=$randomBetweenAnswer
+counter=0 #$randomBetweenAnswer
+randomBetween $counter $LENGTH 1
+echo randomBetweenAnswer=$randomBetweenAnswer
+
+if [[ ! -z "$1" ]]; then
+    echo "content="$1
+    content=$1
+else
+    # echo "content="$1
+    content=""
+fi
+
+if [[ ! -z "$1" ]] || [[ ! -z "$2" ]]; then
+    echo "content="$1
+    content=$1
+    echo "counter="$2
+    counter=$2
+else
+    # echo "content="
+    content=""
+    # echo "counter="0
+    counter=0
+fi
+
 while [[ $counter -lt $LENGTH ]]
     do
     for relay in $RELAYS; do
-        #while [[ $counter -lt $limit ]]
-           echo counter=$counter
-           echo limit=$limit
-           echo randomBetweenAnswer=$randomBetweenAnswer
-           secret=$(echo -en "$counter" | openssl dgst -sha256)
-           echo secret=$secret
-           touch $secret && echo $secret > $secret
-           echo $relay
-           if hash nostril; then
-               if hash nostcat; then
-                   nostril --sec $secret  --envelope --content "" |  nostcat -u $relay
-               else
-                   make -C deps/nostcat/ rustup-install cargo-install
-                   nostril --sec $secret  --envelope --content "" |  nostcat -u $relay
-               fi
+       echo counter=$counter
+       # echo randomBetweenAnswer=$randomBetweenAnswer
+       secret=$(echo -en "$counter" | openssl dgst -sha256)
+       echo secret=$secret
+       touch `pwd`/keys/$secret && echo $secret > `pwd`/keys/$secret
+       echo $relay
+       if hash nostril; then
+           if hash nostcat; then
+               nostril --sec $secret --kind 0 --content "{ name: '$counter', about: '$counter', picture: 'https://robohash.org/$counter'}"
+
+               nostril --sec $secret --kind 1 \
+                   --envelope -t "$counter" -t "$relay" \
+                   --content "$content" --created-at $(date +%s)
+               nostril --sec $secret --kind 1 --envelope  -t "$counter" -t "$relay" \
+                   --content "$content" --created-at $(date +%s) |  nostcat -u $relay
            else
-               make nostril
+               make -C deps/nostcat/ rustup-install cargo-install
+               nostril --sec $secret --kind 1 --envelope  -t "$counter" -t "$relay" \
+                   --content "$content" --created-at $(date +%s)
+               nostril --sec $secret --kind 1 --envelope  -t "$counter" -t "$relay" \
+                   --content "$content" --created-at $(date +%s) |  nostcat -u $relay
            fi
-           ((counter++))
+
+       else
+           make nostril
+       fi
+       ((counter++))
     done
 done
 echo All done
