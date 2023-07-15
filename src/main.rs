000003e2d152b1b157fdda4ef27d02145f5f0fc8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-07-15 19:06:25 -0400

    03:00009549:

--- src/main.rs
+++ src/main.rs
@@ -209,10 +209,14 @@ fn main() -> io::Result<()> {
                 .output()
                 .expect("failed to execute process")
         };
+    let gnostr_event = String::from_utf8(event.stdout)
+    .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+    .unwrap();
 
 
     let duration = time::get_time() - start;
     println!("Success! Generated commit {} in {} seconds", hash, duration.num_seconds());
+    println!("Success! Generated gnostr event {} in {} seconds", gnostr_event, duration.num_seconds());
     Ok(())
 
 }
