00000ecd05aaff3d810eacee8ed3f7e143fc49dd
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-07-22 21:18:59 -0400

    01:00004683:gnostr

--- src/main.rs
+++ src/main.rs
@@ -101,6 +101,38 @@ fn main() -> io::Result<()> {
     if repo.state() != RepositoryState::Clean {
 
     //println!("clean {:?}", repo.state());
+    let repo_path =
+        if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(["/C", "cd"])
+                .output()
+                .expect("failed to execute process")
+        } else
+        if cfg!(target_os = "macos"){
+        Command::new("sh")
+                .arg("-c")
+                .arg("pwd")
+                .output()
+                .expect("failed to execute process")
+        } else
+        if cfg!(target_os = "linux"){
+        Command::new("sh")
+                .arg("-c")
+                .arg("pwd")
+                .output()
+                .expect("failed to execute process")
+        } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("pwd")
+                .output()
+                .expect("failed to execute process")
+        };
+
+    let path = String::from_utf8(repo_path.stdout)
+    .map_err(|non_utf8| String::from_utf8_lossy(non_utf8.as_bytes()).into_owned())
+    .unwrap();
+    println!("path={:?}", path);
 
     let repo_state =
         if cfg!(target_os = "windows") {
